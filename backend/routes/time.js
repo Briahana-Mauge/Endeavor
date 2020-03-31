2a9591ebdf21615e4f81c461a7871a921c8dea15
Bpmauge <briahanamauge@pursuit.org>
2020-03-30 21:20:11 -0400

    skeleton route for time

---
+++ backend/routes/time.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+const db = require('../db/db');
+
+router.get('/past/:volunteer_id', async (req, res) => {
+    try {
+        let allBankedHours = await db.any("SELECT banked_hours FROM volunteer_hours WHERE volunteer = 'volunteer'");
+
+        res.json({
+            users: allBankedHours,
+            message: "Success"
+        });
+    } catch (error) {
+        res.json({
+            message: "Error"
+        });
+        console.log(error);
+    }
+});
+
+router.get('/upcoming/:volunteer_id', async (req, res) => {
+    try {
+        let allPlannedHours = await db.any("SELECT planned_time FROM volunteer_hours WHERE volunteer = 'volunteer'");
+
+        res.json({
+            users: allPlannedHours,
+            message: "Success"
+        });
+    } catch (error) {
+        res.json({
+            message: "Error"
+        });
+        console.log(error);
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        let allHours = await db.any("SELECT banked_hours FROM volunteer_hours");
+
+        res.json({
+            users: allHours,
+            message: "Success"
+        });
+    } catch (error) {
+        res.json({
+            message: "Error"
+        });
+        console.log(error);
+    }
+});
+
+module.exports = router;
