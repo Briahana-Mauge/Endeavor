e3eff6b9148bd15a851315fbc78daafeeccf4f03
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-04-02 02:19:57 -0400

    revise handleError comments for clarity, fix declared var bug

--- backend/helpers/handleError.js
+++ backend/helpers/handleError.js
@@ -1,13 +1,13 @@
 /*
 ANIME BENSALEM, BRIAHANA MAUGÉ, JOSEPH P. PASAOA
 Server Error Handling Helper | Capstone App (Pursuit Volunteer Mgr)
-*/
 
 
-/*
 USAGE
-to instantiate custom error, format: throw new Error(`<error code>__error: <msg>`);
-example: throw new Error(`404__error: volunteer ${volunteerId} not found`);
+to instantiate custom error, use format:
+  throw new Error(`<error code>__error: <msg>`);
+example:
+  throw new Error(`404__error: volunteer ${volunteerId} not found`);
 */
 
 
@@ -23,13 +23,16 @@ const errorResponse = (res, code, errorMsg) => {
 
 
 const handleError = (err, req, res, next) => {
-  // per documentation, checking for present headers and if so, passing
+  /* STAGE 1/5:
+  Per documentation, checking for present headers and if so, pass to default Express error handler
+  https://expressjs.com/en/guide/error-handling.html#the-default-error-handler
+  */
   if (res.headersSent) {
     console.log("err: res headers already exist. passing error to express");  // msg to developer
     return next(err);
   } else {
 
-    // specific-case (database response) errors
+    /* STAGE 2/5: intercept database response errors and customize error msgs */
     if (err.code === "23503") {
       err.message = `403__error: reference error in database lookup`;
     } else if (err.code === "23505" && err.message.includes("violates unique constraint")) {
@@ -38,22 +41,20 @@ const handleError = (err, req, res, next) => {
       err.message = `404__error: specific record does not exist`;
     }
 
-    // catch and parse error.messages for relevant http code
+    /* STAGE 3/5: split custom error.messages for relevant http code */
     let [ code, msg = code ] = err.message.split('__');
 
-    // if "__" wasn't found, error was not thrown from a customized instance
+    /* STAGE 4/5: if "__" wasn't found, error was not thrown from a customized instance and will send response here */
     if (msg === code) {
       errorResponse(res, 500, `(back) error: ${msg}`);
     } else {
 
-      // create and send response
+      /* STAGE 5/5: custom error received. format and send response */
       res.status(parseInt(code));
       const codeClass = parseInt(code).toString()[0];
-      if (codeClass === '4') {
-        errorLocation = "(front)";
-      } else {
-        errorLocation = "(back)";
-      }
+      const errorLocation = codeClass === '4'
+        ? "(front)"
+        : "(back)";
       errorResponse(res, code, `${errorLocation} ${msg}`);
     }
   }
