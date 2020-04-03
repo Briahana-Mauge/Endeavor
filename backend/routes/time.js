4550eddb7d87202de63099db1cbed84af0d41819
Bpmauge <briahanamauge@pursuit.org>
2020-04-02 21:06:01 -0400

    add helper functions: process imput and error handler

--- backend/routes/time.js
+++ backend/routes/time.js
@@ -1,45 +1,47 @@
-var express = require('express');
-var router = express.Router();
+/*
+ANIME BENSALEM, BRIAHANA MAUGÉ, JOSEPH P. PASAOA
+Time Route Handler | Capstone App (Pursuit Volunteer Mgr)
+*/
+
+const express = require('express');
+const router = express.Router();
 const db = require('../db/db');
+
+const handleError = require('../helpers/handleError');
+const processInput = require('../helpers/processInput');
 const timeQueries = require('../queries/time');
 
 //Gets the number of hours banked by a volunteer
-router.get('/past/:v_id', async (req, res) => {
+router.get('/past/:v_id', async (req, res, next) => {
     try {
-        const vId = req.params.v_id;
+        const vId = processInput(req.params.v_id, "idNum", "volunteer id");
         let allBankedHours = await timeQueries.allBankedHours(vId);
 
         res.json({
             users: allBankedHours,
             message: "Success"
         });
-    } catch (error) {
-        res.json({
-            message: "Error"
-        });
-        console.log(error);
+    } catch (err) {
+        handleError(err, req, res, next);
     }
 });
 
 //Gets the number of hours planned by a volunteer
-router.get('/upcoming/:v_id', async (req, res) => {
+router.get('/upcoming/:v_id', async (req, res, next) => {
     try {
-        const vId = req.params.v_id;
+        const vId = processInput(req.params.v_id, "idNum", "volunteer id");
         let allPlannedHours = await timeQueries.allPlannedHours(vId);
         res.json({
             users: allPlannedHours,
             message: "Success"
         });
-    } catch (error) {
-        res.json({
-            message: "Error"
-        });
-        console.log(error);
+    } catch (err) {
+        handleError(err, req, res, next);
     }
 });
 
 //Gets the number of hours by all volunteers
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
     try {
         let allHours = await timeQueries.allHours();
 
@@ -47,11 +49,8 @@ router.get('/', async (req, res) => {
             users: allHours,
             message: "Success"
         });
-    } catch (error) {
-        res.json({
-            message: "Error"
-        });
-        console.log(error);
+    } catch (err) {
+        handleError(err, req, res, next);
     }
 });
 
