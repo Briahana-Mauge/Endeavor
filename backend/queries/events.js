6566a68d9e97e7f32ab63f75ff46593524e41a43
Bpmauge <briahanamauge@pursuit.org>
2020-04-07 22:01:27 -0400

    rename class & classes to cohort & cohorts

--- backend/queries/events.js
+++ backend/queries/events.js
@@ -14,7 +14,7 @@ const db = require('../db/db');
 const getAllEvents = async () => {
   const selectQuery = `
   SELECT events.event_id, events.topic, events.event_start, events.event_end, events.description, events.location, 
-    events.instructor, events.number_of_volunteers AS volunteers_needed, ARRAY_AGG (DISTINCT classes.class) AS class,
+    events.instructor, events.number_of_volunteers AS volunteers_needed, ARRAY_AGG (DISTINCT cohorts.cohort) AS cohort,
     ARRAY_AGG ( 
       DISTINCT
       CASE 
@@ -25,12 +25,12 @@ const getAllEvents = async () => {
 
   FROM events
 
-  INNER JOIN classes ON classes.class_id = events.attendees
+  INNER JOIN cohorts ON cohorts.cohort_id = events.attendees
   INNER JOIN event_volunteers ON event_volunteers.eventv_id = events.event_id
   INNER JOIN volunteers ON volunteers.v_id = event_volunteers.volunteer_id
                  
   GROUP BY  events.event_id, events.topic, events.event_start, events.event_end, events.description, events.location, 
-    events.instructor, events.number_of_volunteers, classes.class     
+    events.instructor, events.number_of_volunteers, cohorts.cohort     
 
   ORDER BY (
     CASE WHEN DATE(event_start) > now()
@@ -46,7 +46,7 @@ const getAllEvents = async () => {
 const getSingleEvent = async (eId) => {
   const selectQuery = `
   SELECT events.event_id, events.topic, events.event_start, events.event_end, events.description, events.location, 
-    events.instructor, events.number_of_volunteers AS volunteers_needed, ARRAY_AGG (DISTINCT classes.class) AS class,
+    events.instructor, events.number_of_volunteers AS volunteers_needed, ARRAY_AGG (DISTINCT cohorts.cohort) AS cohort,
     ARRAY_AGG ( 
       DISTINCT
       CASE 
@@ -57,14 +57,14 @@ const getSingleEvent = async (eId) => {
 
   FROM events
 
-  INNER JOIN classes ON classes.class_id = events.attendees
+  INNER JOIN cohorts ON cohorts.cohort_id = events.attendees
   INNER JOIN event_volunteers ON event_volunteers.eventv_id = events.event_id
   INNER JOIN volunteers ON volunteers.v_id = event_volunteers.volunteer_id
        
   WHERE events.event_id = $/eId/
 
   GROUP BY  events.event_id, events.topic, events.event_start, events.event_end, events.description, events.location, 
-    events.instructor, events.number_of_volunteers, classes.class     
+    events.instructor, events.number_of_volunteers, cohorts.cohort    
 
   ORDER BY (
     CASE WHEN DATE(event_start) > now()
@@ -80,17 +80,17 @@ const getSingleEvent = async (eId) => {
 const getAllEventsAdmin = async () => {
   const selectQuery = `
   SELECT events.event_id, events.topic, events.event_start, events.event_end, events.description, events.location, 
-    events.instructor, events.number_of_volunteers AS volunteers_needed, ARRAY_AGG (DISTINCT classes.class) AS class, 
+    events.instructor, events.number_of_volunteers AS volunteers_needed, ARRAY_AGG (DISTINCT cohorts.cohort) AS cohort, 
     ARRAY_AGG ( DISTINCT volunteers.v_first_name || ' ' || volunteers.v_last_name) AS volunteers
 
   FROM events
 
-  INNER JOIN classes ON classes.class_id = events.attendees
+  INNER JOIN cohorts ON cohorts.cohort_id = events.attendees
   INNER JOIN event_volunteers ON event_volunteers.eventv_id = events.event_id
   INNER JOIN volunteers ON volunteers.v_id = event_volunteers.volunteer_id
                  
   GROUP BY  events.event_id, events.topic, events.event_start, events.event_end, events.description, events.location, 
-    events.instructor, events.number_of_volunteers, classes.class
+    events.instructor, events.number_of_volunteers, cohorts.cohort
          
   ORDER BY (CASE WHEN DATE(event_start) > now()
     THEN 1
@@ -106,19 +106,19 @@ return await db.any(selectQuery);
 const getSingleEventAdmin = async (eId) => {
   const selectQuery = `
   SELECT events.event_id, events.topic, events.event_start, events.event_end, events.description, events.location, 
-  events.instructor, events.number_of_volunteers AS volunteers_needed, ARRAY_AGG (DISTINCT classes.class) AS class, 
+  events.instructor, events.number_of_volunteers AS volunteers_needed, ARRAY_AGG (DISTINCT cohorts.cohort) AS cohort, 
   ARRAY_AGG ( DISTINCT volunteers.v_first_name || ' ' || volunteers.v_last_name) AS volunteers
 
   FROM events
 
-  INNER JOIN classes ON classes.class_id = events.attendees
+  INNER JOIN cohorts ON cohorts.cohort_id = events.attendees
   INNER JOIN event_volunteers ON event_volunteers.eventv_id = events.event_id
   INNER JOIN volunteers ON volunteers.v_id = event_volunteers.volunteer_id
     
   WHERE events.event_id = $/eId/
 
   GROUP BY  events.event_id, events.topic, events.event_start, events.event_end, events.description, events.location, 
-    events.instructor, events.number_of_volunteers, classes.class
+    events.instructor, events.number_of_volunteers, cohorts.cohort
   `
   return await db.one(selectQuery, {eId});
 }
