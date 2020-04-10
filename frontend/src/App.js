c3170b345124198dc4947a4fec72d93164c98db7
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-04-10 04:57:46 -0400

    add App.scss import connection to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Switch, Route, useHistory } from 'react-router-dom';
 import axios from 'axios';
 
+import './App.scss';
 import LoginSignup from './Components/LoginSignup';
 import ErrorFeedback from './Components/ErrorFeedback';
 import VolunteerSearch from './Components/VolunteerSearch';
