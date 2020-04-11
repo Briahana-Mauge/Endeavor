9fc2271e3644e3f57c2cb7208361a9daf4de2801
Bpmauge <briahanamauge@pursuit.org>
2020-04-10 22:22:04 -0400

    got rid of errors: rermove nested a tag and changed class to className

--- frontend/src/Components/VolunteerSearchCard.jsx
+++ frontend/src/Components/VolunteerSearchCard.jsx
@@ -3,21 +3,21 @@ import { Link} from 'react-router-dom';
 
 const VolunteerSearchCard = (props) => {
     return (
-        <div class="card" style={{ width: 400 }}>
-            <img class="card-img-top" src={props.volunteer.v_picture} alt={`${props.volunteer.v_first_name} ${props.volunteer.v_last_name}'s picture`} />
-            <div class="card-body">
-                <h4 class="card-title">{props.volunteer.v_first_name} {props.volunteer.v_last_name}</h4>
-                <p class="card-text">{props.volunteer.title} at {props.volunteer.company}</p>
-                <p class="card-text">{props.volunteer.v_email}</p>
+        <div className="card" style={{ width: 400 }}>
+            <img className="card-img-top" src={props.volunteer.v_picture} alt={`${props.volunteer.v_first_name} ${props.volunteer.v_last_name}'s picture`} />
+            <div className="card-body">
+                <h4 className="card-title">{props.volunteer.v_first_name} {props.volunteer.v_last_name}</h4>
+                <p className="card-text">{props.volunteer.title} at {props.volunteer.company}</p>
+                <p className="card-text">{props.volunteer.v_email}</p>
                 <h5>Skills:</h5>
                 <ul>
-                    <p class="card-text">{props.volunteer.skills[0]}</p>
-                    <p class="card-text">{props.volunteer.skills[1]}</p>
-                    <p class="card-text">{props.volunteer.skills[2]}</p>
+                    <p className="card-text">{props.volunteer.skills[0]}</p>
+                    <p className="card-text">{props.volunteer.skills[1]}</p>
+                    <p className="card-text">{props.volunteer.skills[2]}</p>
                 </ul>
                 <h5>Next Event: {props.volunteer.topics[0]} (make this a link)</h5>
-                <Link to={`/volunteers/${props.volunteer.v_id}`}>
-                    <a href="#" class="btn btn-primary">See Profile</a>
+                <Link to={`/volunteers/${props.volunteer.v_id}`} className="btn btn-primary">
+                    <button>See Profile</button>
                     </Link>
             </div>
         </div>
