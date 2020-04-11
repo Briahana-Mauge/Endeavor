73e8702c65f1d5be7d13345b0fac830246bbd905
Bpmauge <briahanamauge@pursuit.org>
2020-04-10 22:13:21 -0400

    rearrange where data is passed to child card component and add additional cases in the front end for filtering

--- frontend/src/Components/VolunteerSearch.jsx
+++ frontend/src/Components/VolunteerSearch.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import VolunteerSearchCard from './VolunteerSearchCard';
-import { Link, Route, Switch } from 'react-router-dom';
 
 
+// React Hooks
 // const VolunteerSearch = () => {
 //     const [search, setSearch] = useState('');
 //     const [results, setResults] = useState([]);
@@ -58,89 +58,50 @@ import { Link, Route, Switch } from 'react-router-dom';
 
 
 
-
-
-
-
-
-// setSearch('newstring');
-
-
-// this.setState = ({
-//search: new variable
-// })
-
-// [e.target.name]: e.target.value
-
-// }
-
+//Class component
 class VolunteerSearch extends React.Component {
     constructor() {
         super();
         this.state = {
             search: '',
             loading: true,
-            results: []
+            results: [],
+            filter: '',
+            submitted: false
         }
     }
-    componentDidMount = async () => {
-        let basic = await axios.get(`/api/volunteers/all`);
+    componentDidMount = () => {
+
+        this.getAllVolunteers();
+    }
+
+    getAllVolunteers = async () => {
+
+        let { results, filter, search } = this.state
+        let basic = ''
+        if (filter === '') {
+            basic = await axios.get(`/api/volunteers/all/`);
+
+        } else if (filter === 'v_email') {
+            basic = await axios.get(`/api/volunteers/all/?v_email=${search}`);
+
+        } else if (filter === 'company') {
+            basic = await axios.get(`/api/volunteers/all/?company=${search}`);
+
+        }
+        else if (filter === 'skill') {
+            basic = await axios.get(`/api/volunteers/all/?skill=${search}`);
+
+        } else {
+            basic = await axios.get(`/api/volunteers/all/?name=${search}`);
+        }
+
+
         this.setState({
             results: basic.data.payload
         })
     }
 
-    getAllVolunteers = () => {
-        let { results } = this.state
-        console.log(results)
-        return (
-            <div>
-                {results.map(volunteer => {
-                    return (
-
-                        <div key={volunteer.v_id}>
-                            <VolunteerSearchCard volunteer={volunteer} />
-
-                        </div>
-
-                    )
-                })}
-            </div>
-
-        )
-
-
-
-
-        try {
-            // event.preventDefault();
-            // let basic = await axios.get(`/api/volunteers/all`);
-            let newResults = []
-            let basic = 0
-            // console.log(basic.data.payload);
-            let volunteers = basic.data.payload
-            for (let i = 0; i < volunteers.length; i++) {
-                newResults.push({
-
-                    picture: volunteers[i].v_picture,
-                    name: volunteers[i].v_first_name + ' ' + volunteers[i].v_last_name,
-                    company: volunteers[i].company,
-                    title: volunteers[i].title,
-                    email: volunteers[i].v_email,
-                    skills: 'none',
-                    nextEvent: 'none'
-                })
-            }
-            this.setState({
-                results: newResults
-            })
-            // console.log('results', newResults)
-
-        } catch (error) {
-            console.log(error)
-        }
-
-    }
     handleInput = (event) => {
         const { name, value } = event.target
         this.setState({
@@ -150,27 +111,44 @@ class VolunteerSearch extends React.Component {
     }
     handleSubmit = async (event) => {
         event.preventDefault();
-       this.setState({
+        this.setState({
             loading: false,
-            search: ''
         })
-         const { search } = this.state;
-        let volunteers = [];
-        let newResults = [];
-        console.log('onclick works')
-        
+
+        this.getAllVolunteers();
+
+
     }
     render = () => {
-        // console.log(this.state.results)
+
+        const { results } = this.state
         return (
             <div className="Search">
                 <form>
-                <input type='text' name = 'search 'placeholder='Search' onChange = {this.handleInput} value = {this.setState.search}/>
-                <input type='button' value='Send'  onClick={this.handleSubmit}/>
-</form>
-                <Switch>
-                    <Route exact path="/volunteers/search" render={this.getAllVolunteers} />
-                </Switch>
+                    <input type='text' name='search' placeholder='Search' onChange={this.handleInput} value={this.setState.search} />
+                    <input type='button' value='Send' onClick={this.handleSubmit} />
+                    <select className='filter' name='filter' onChange={this.handleInput}>
+                        <option value={false} key='null'>Choose a search filter</option>
+                        <option value='name' key='name'>Name</option>
+                        <option value='v_email' key='v_email'>Email</option>
+                        <option value='company' key='company'>Company</option>
+                        <option value='skill' key='skills'>Skill</option>
+                    </select>
+                </form>
+                <div>
+                    {results.map(volunteer => {
+                        return (
+
+                            <div key={volunteer.v_id}>
+                                <VolunteerSearchCard volunteer={volunteer} />
+
+                            </div>
+
+                        )
+                    })}
+                </div>
+
+
             </div>
         );
     }
