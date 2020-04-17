77c1dec87bce1323f266e41f164f08ab5aab61b1
Bpmauge <briahanamauge@pursuit.org>
2020-04-16 22:36:42 -0400

    add event conditionals for if user is admin or not

--- frontend/src/Components/Events.jsx
+++ frontend/src/Components/Events.jsx
@@ -18,8 +18,15 @@ export default function EventSearch(props) {
     useEffect(() => {
         const getAllEvents = async () => {
             try {
-                const { data } = await axios.get(`/api/events/all/?${filter}=${search}&${dateFilter}=${dateFilter}`);
-                setResults(data.payload);
+                if (props.loggedUser && props.loggedUser.a_id) {
+                    const { data } = await axios.get(`/api/events/admin/all/?${filter}=${search}&${dateFilter}=${dateFilter}`);
+                    setResults(data.payload);
+                }
+                else {
+                    const { data } = await axios.get(`/api/events/all/?${filter}=${search}&${dateFilter}=${dateFilter}`);
+                    setResults(data.payload);
+                }
+
             } catch (err) {
                 setFeedback(err)
             }
@@ -49,37 +56,40 @@ export default function EventSearch(props) {
 
 
     return (
-        <div className=''>
-            <form className='form-inline' onSubmit={handleSubmit}>
-                <input className='form-control mb-2 mr-sm-2 w-25' type='text' placeholder='Search' value={search} onChange={e => setSearch(e.target.value)} />
+        // {(props.loggedUser && props.loggedUser.a_id)? 
+//     :
+// }
+<div className=''>
+    <form className='form-inline' onSubmit={handleSubmit}>
+        <input className='form-control mb-2 mr-sm-2 w-25' type='text' placeholder='Search' value={search} onChange={e => setSearch(e.target.value)} />
 
-                <select className='form-control mb-2 mr-sm-2' value={filter} onChange={e => setFilter(e.target.value)}>
-                    <option value=''>Choose a search filter</option>
-                    <option value='topic'>Event Name</option>
-                    <option value='v_name'>Volunteer</option>
-                    <option value='instructor'>Instructor</option>
-                </select>
+        <select className='form-control mb-2 mr-sm-2' value={filter} onChange={e => setFilter(e.target.value)}>
+            <option value=''>Choose a search filter</option>
+            <option value='topic'>Event Name</option>
+            <option value='v_name'>Volunteer</option>
+            <option value='instructor'>Instructor</option>
+        </select>
 
-                <select className='form-control mb-2 mr-sm-2' value={dateFilter} onChange={e => setDateFilter(e.target.value)}>
-                    <option value=''>Any date</option>
-                    <option value='upcoming'>Upcoming events</option>
-                    <option value='past'>Past events</option>
-                </select>
+        <select className='form-control mb-2 mr-sm-2' value={dateFilter} onChange={e => setDateFilter(e.target.value)}>
+            <option value=''>Any date</option>
+            <option value='upcoming'>Upcoming events</option>
+            <option value='past'>Past events</option>
+        </select>
 
-                <button className='btn btn-primary mb-2'>Send</button>
-            </form>
+        <button className='btn btn-primary mb-2'>Send</button>
+    </form>
 
-            <div className='d-flex flex-wrap justify-content-around'>
-                {results.map(event => {
-                    return (
-                        <div key={event.event_id}>
-                            <EventsSearchCard event={event} delete={handleDelete} /* edit = {} *//>
-                        </div>
-                    )
-                })}
-            </div>
+    <div className='d-flex flex-wrap justify-content-around'>
+        {results.map(event => {
+            return (
+                <div key={event.event_id}>
+                    <EventsSearchCard role = {props.loggedUser.admin} event={event} delete={handleDelete} /* edit = {} */ />
+                </div>
+            )
+        })}
+    </div>
 
 
-        </div>
+</div>
     );
 }
--- frontend/src/Components/EventsCard.jsx
+++ frontend/src/Components/EventsCard.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 // import { Link } from 'react-router-dom';
 
 const EventsSearchCard = (props) => {
+  if(props.event.volunteers[0] !== null){
+    console.log('not empty',props.event.volunteers.join(', '))
+  }
     return (
         <div className='card' style={{ width: 400 }}>
-            <button onClick = {e => props.delete(e, props.event.event_id)}>Delete</button>
+            {props.role ?
+                <button onClick={e => props.delete(e, props.event.event_id)}>Delete</button>
+                : null
+            }
+            {/* // <button onClick = {e => props.delete(e, props.event.event_id)}>Delete</button> */}
             {/* <button onClick = {e => props.edit(e, props.event.event_id)}>Edit</button> */}
 
             <div className='card-body'>
@@ -12,13 +19,20 @@ const EventsSearchCard = (props) => {
                 <p>{new Date(props.event.event_start).toLocaleString()} - {new Date(props.event.event_end).toLocaleString()}</p>
                 <p className='card-text'>
                     Hosted by: {props.event.instructor}
-                    </p>
+                </p>
                 <p className='card-text'>{props.event.description} </p>
                 <p className='card-text'>Class: {props.event.cohort} </p>
                 {
-                (props.event.volunteers) ?
-                <p className='card-text'>Volunteers: {props.event.volunteers}</p>
-                : <p className='card-text'>Volunteers: TBA</p>
+                    (props.event.volunteers[0] === null) ?
+                        <p className='card-text'>Volunteers: TBA</p>
+                        :<p className='card-text'>Volunteers: {props.event.volunteers.join(', ')}
+                        {/* {props.event.volunteers.forEach(e =>{
+                        
+                            console.log(e)
+                            return <p className='card-text'>Volunteers: {e} </p>;
+                        })} */}
+                        </p>
+                        
                 }
 
 
