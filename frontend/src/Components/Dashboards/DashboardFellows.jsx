556663f6ac08727f71cff797ca3f58c0fc849616
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-04-20 04:42:29 -0400

    add fellows dashboard

---
+++ frontend/src/Components/Dashboards/DashboardFellows.jsx
@@ -0,0 +1,32 @@
+/*
+ANIME BENSALEM, BRIAHANA MAUGÉ, JOSEPH P. PASAOA
+DashboardFellows Component | Capstone App (Pursuit Volunteer Mgr)
+*/
+
+
+/* IMPORTS */
+import React from 'react'
+
+
+const DashboardFellows = () => {
+  return (
+    <div className="dashboard mt-4">
+      
+      <h2>Fellow's Dashboard</h2><br />
+
+      <strong>Events</strong><br />
+      Upcoming events<br />
+      Newly added events<br />
+      Events RSVPed for<br /><br />
+
+      <strong>Mentoring</strong><br />
+      Mentor(s)<br /><br />
+
+      <strong>Some My Profile Data</strong><br />
+      Link to edit profile<br /><br />
+
+    </div>
+  );
+}
+
+export default DashboardFellows;
