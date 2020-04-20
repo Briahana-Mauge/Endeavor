0028fa7a595f5a6c2bbeac8378295a818e11d978
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-04-20 04:42:02 -0400

    add staff dashboard

---
+++ frontend/src/Components/Dashboards/DashboardStaff.jsx
@@ -0,0 +1,31 @@
+/*
+ANIME BENSALEM, BRIAHANA MAUGÉ, JOSEPH P. PASAOA
+DashboardStaff Component | Capstone App (Pursuit Volunteer Mgr)
+*/
+
+
+/* IMPORTS */
+import React from 'react'
+
+
+const DashboardStaff = () => {
+  return (
+    <div className="dashboard mt-4">
+      
+      <h2>Staff's Dashboard</h2><br />
+
+      <strong>All new volunteer signups</strong><br />
+      Links to new volunteer profiles<br /><br />
+
+      <strong>Today's events</strong><br />
+      Links to google or event page<br />
+      Create new event button for pending approval by Admin<br /><br />
+
+      <strong>Data at hand</strong><br />
+      Volunteer hours (by last 7 days? total? banked?)<br /><br />
+
+    </div>
+  );
+}
+
+export default DashboardStaff;
