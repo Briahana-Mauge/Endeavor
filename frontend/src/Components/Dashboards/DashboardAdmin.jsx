dbee75f2c1e244f07358868a74862978d58547f8
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-05-04 01:34:45 -0400

    complete retool of DashboardAdmin with NewVolunteersDash component

--- frontend/src/Components/Dashboards/DashboardAdmin.jsx
+++ frontend/src/Components/Dashboards/DashboardAdmin.jsx
@@ -8,59 +8,41 @@ DashboardAdmin Component | Capstone App (Pursuit Volunteer Mgr)
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import VolunteerPreviewCard from '../VolunteerPreviewCard';
 import EventsDashAdmin from './EventsDash/EventsDashAdmin';
+import NewVolunteersDash from './NewVolunteersDash/NewVolunteersDash';
 import EventCard from '../EventCard';
 
 
 const DashboardAdmin = (props) => {
     const { setFeedback, loggedUser } = props;
 
-    const [ newVolunteersList, setNewVolunteersList ] = useState([]);
+    const [ newVolunteers, setNewVolunteers ] = useState([]);
     const [ eventsObj, setEventsObj ] = useState({ todays: [], importants: [], upcomings: [] });
     const [ showEvent, setShowEvent ] = useState(false);
     const [ targetEvent, setTargetEvent ] = useState({});
     const [ reloadDashboard, setReloadDashboard ] = useState(false);
 
-
     useEffect(() => {
-        const getNewVolunteer = async () => {
-          try {
-              const {data} = await axios.get('/api/volunteers/new');
-              setNewVolunteersList(data.payload);
-          } catch (err) {
-              setFeedback(err)
-          }
+        const getNewVolunteers = () => {
+          axios.get('/api/volunteers/new')
+            .then(res => setNewVolunteers(res.data.payload))
+            .catch(err => setFeedback(err));
+        }
+        const getEvents = () => {
+          axios.get('/api/events/dashboard/admin')
+            .then(res => setEventsObj(res.data.payload))
+            .catch(err => setFeedback(err));
         }
 
-        const getEvents = async () => {
-          try {
-              const {data} = await axios.get('/api/events/dashboard/admin');
-              setEventsObj(data.payload);
-          } catch (err) {
-              setFeedback(err)
-          }
-        }
-
-        getNewVolunteer();
+        getNewVolunteers();
         getEvents();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [reloadDashboard]);
+    }, [ reloadDashboard, setFeedback ]);
 
-
-    const acceptVolunteer = async (id) => {
-        try {
-            await axios.patch(`/api/volunteers/confirm/${id}`);
-            setReloadDashboard(!reloadDashboard);
-        } catch (err) {
-            setFeedback(err)
-        }
-    }
 
     const hideEvent = () => {
         setTargetEvent({});
         setShowEvent(false);
-    }
+    };
 
 
     // PRE-RETURN (package drilled props)
@@ -69,24 +51,12 @@ const DashboardAdmin = (props) => {
       setShowEvent,
       targetEvent,
       setTargetEvent
-    }
-
-
-    const listNewVolCardPointers = [];
-    const listNewVolCards = newVolunteersList.map(( volunteer, index ) => {
-      console.log(index === 0)
-        listNewVolCardPointers.push(index === 0
-          ? <li data-target="#newVolunteersSlideshow" data-slide-to={`${index}`} className="active" key={index}></li>
-          : <li data-target="#newVolunteersSlideshow" data-slide-to={`${index}`} key={index}></li>
-        );
-        return(
-          <div className={index === 0 ? "carousel-item active" : "carousel-item"} key={volunteer.v_first_name+volunteer.v_last_name_volunteer_id}>
-              <VolunteerPreviewCard volunteer={volunteer} acceptVolunteer={acceptVolunteer}/>
-          </div>
-        );
-    });
-
-
+    };
+    const newVolunteersProps = {
+      reloadDashboard,
+      setReloadDashboard,
+      setFeedback
+    };
 
 
     return (
@@ -97,54 +67,25 @@ const DashboardAdmin = (props) => {
             </div>
 
             <div className="col-12 col-md-7">
-              <div id="newVolunteersSlideshow" className="carousel slide" data-ride="carousel" data-interval="false">
-                <ol className="carousel-indicators">
-                  {listNewVolCardPointers}
-                </ol>
-                <div className="carousel-inner">
-                  {listNewVolCards}
-                </div>
-                <a className="carousel-control-prev" href="#newVolunteersSlideshow" role="button" data-slide="prev">
-                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#newVolunteersSlideshow" role="button" data-slide="next">
-                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span className="sr-only">Next</span>
-                </a>
-              </div>
+              <NewVolunteersDash newVolunteers={newVolunteers} {...newVolunteersProps} />
             </div>
           </div>
 
-        {/* <hr /> */}
-        {/* <h3>Upcoming Events:</h3>
-        <div className='d-flex flex-wrap'>
-            {
-                eventsObj.todays.map(event => <EventPreviewCard
-                        key={event.event_id + event.event_end + event.event_start}
-                        loggedUser={loggedUser}
-                        event={event}
-                        setShowEvent={setShowEvent}
-                        targetEvent={targetEvent}
-                        setTargetEvent={setTargetEvent}
-                    />)
-            }
-        </div> */}
-
         {
-            showEvent
+          showEvent
             ? <EventCard
-                    loggedUser={loggedUser}
-                    event={targetEvent}
-                    setFeedback={setFeedback}
-                    reloadParent={reloadDashboard}
-                    setReloadParent={setReloadDashboard}
-                    hideEvent={hideEvent}
+                loggedUser={loggedUser}
+                event={targetEvent}
+                setFeedback={setFeedback}
+                reloadParent={reloadDashboard}
+                setReloadParent={setReloadDashboard}
+                hideEvent={hideEvent}
               />
             : null
         }
         </div>
     )
 }
+
 
 export default DashboardAdmin;
