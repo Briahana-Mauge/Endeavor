eafd3744cfa9c485379d09dd3114514a11e09a31
Bpmauge <briahanamauge@pursuit.org>
2020-05-09 08:41:02 -0400

    start game chart

--- frontend/src/Components/Dashboards/DashboardVolunteers.jsx
+++ frontend/src/Components/Dashboards/DashboardVolunteers.jsx
@@ -13,165 +13,147 @@ import EventsDashVolunteers from './EventsDash/EventsDashVolunteers';
 import EventCard from '../EventCard';
 // import EventPreviewCard from '../EventPreviewCard';
 
+import { Bar, Pie, Line } from 'react-chartjs-2';
+
 const Dashboard = (props) => {
-    const { setFeedback, loggedUser } = props;
+  const { setFeedback, loggedUser } = props;
 
-    const [eventsObj, setEventsObj] = useState({ upcomings: [], pasts: [], importants: [] });
-    const [showEvent, setShowEvent] = useState(false);
-    const [targetEvent, setTargetEvent] = useState({});
-    const [volunteeredTime, setVolunteeredTime] = useState(0);
-    const [reloadDashboard, setReloadDashboard] = useState(false);
-    // const [eventsList, setEventsList] = useState([]);
-    // const [pastEvents, setPastEvents] = useState(0);
+  const [eventsObj, setEventsObj] = useState({ upcomings: [], pasts: [], importants: [] });
+  const [showEvent, setShowEvent] = useState(false);
+  const [targetEvent, setTargetEvent] = useState({});
+  const [volunteeredTime, setVolunteeredTime] = useState(0);
+  const [reloadDashboard, setReloadDashboard] = useState(false);
 
+  const [barData, setBarData] = useState({
+    labels: ['volunteer hours', 'label 2', 'label 3', 'label 4'],
+    datasets: [
+      {
+        label: 'test label',
+        data: [
+          { volunteeredTime },
+          `${eventsObj.pasts.length}`,
+          73,
+          82
+        ],
+        backgroundColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)'
+        ],
+        borderWidth: 3
+      }
+    ]
+  });
+  const [barOptions, setBarOptions] = useState({
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      },
+      title: {
+        display: true,
+        text: 'Data Orgranized In Bars',
+        fontSize: 25
+      },
+      legend: {
+        display: true,
+        position: 'top'
+      }
+    }
+  });
 
-    const getAllVolunteeredTime = () => {
-      axios.get(`/api/time/hours/${loggedUser.v_id}`)
-        .then(res => setVolunteeredTime(res.data.payload.sum))
+  console.log(eventsObj.pasts.length)
+
+  let dataArr = [volunteeredTime, 2, volunteeredTime, eventsObj.pasts.length,]
+  for(let i = 0; i<4; i++){
+  barData.datasets[0].data[i] = dataArr[i]
+   }
+   
+  // barData.datasets[0].data[0] = volunteeredTime
+
+  const getAllVolunteeredTime = () => {
+    axios.get(`/api/time/hours/${loggedUser.v_id}`)
+      .then(res => setVolunteeredTime(res.data.payload.sum))
+      .catch(err => setFeedback(err));
+  }
+  useEffect(getAllVolunteeredTime, []);
+
+  useEffect(() => {
+    const getEventsData = () => {
+      axios.get(`/api/events/dashboard/volunteer`)
+        .then(res => setEventsObj(res.data.payload))
         .catch(err => setFeedback(err));
     }
-    useEffect(getAllVolunteeredTime, []);
+    getEventsData();
 
-    useEffect(() => {
-        const getEventsData = () => {
-          axios.get(`/api/events/dashboard/volunteer`)
-            .then(res => setEventsObj(res.data.payload))
-            .catch(err => setFeedback(err));
-        }
-        getEventsData();
-    }, [ reloadDashboard, loggedUser.v_id, setFeedback ]);
+  }, [reloadDashboard, loggedUser.v_id, setFeedback]);
 
-    //   useEffect(() => {
-    //     const getEvents = async () => {
-    //         try {
-    //             const { data } = await axios.get(`/api/events/upcoming/volunteer/${props.loggedUser.v_id}?limit=3`);
-    //             setEventsList(data.payload);
-    //         } catch (err) {
-    //             setFeedback(err)
-    //         }
-    //     }
-
-    //     const getImportantEvents = async () => {
-    //         try {
-    //             const { data } = await axios.get(`/api/events/important?limit=3`);
-    //             setImportantEvents(data.payload);
-
-    //         } catch (err) {
-    //             setFeedback(err)
-    //         }
-    //     }
-
-    //     getEvents();
-    //     getImportantEvents();
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, [reloadDashboard]);
-
-    // useEffect(() => {
-    //     const getAllVolunteeredTime = async () => {
-    //         try {
-    //             const { data } = await axios.get(`/api/time/hours/${props.loggedUser.v_id}`);
-    //             setVolunteeredTime(data.payload.sum)
-
-    //         } catch (err) {
-    //             setFeedback(err)
-    //         }
-    //     }
-
-    //     const getNumberOfPastEvents = async () => {
-    //         try {
-    //             const { data } = await axios.get(`/api/events/past/volunteer/${props.loggedUser.v_id}`);
-    //             setPastEvents(data.payload.length)
-    //         } catch (err) {
-    //             setFeedback(err)
-    //         }
-    //     }
-
-    //     getAllVolunteeredTime();
-    //     getNumberOfPastEvents();
-    //     // eslint-disable-next-line react-hooks/exhaustive-deps
-    // }, []);
+  const hideEvent = () => {
+    setTargetEvent({});
+    setShowEvent(false);
+  }
 
 
-    const hideEvent = () => {
-        setTargetEvent({});
-        setShowEvent(false);
-    }
+  // PRE-RETURN (package drilled props)
+  const eventsDashProps = {
+    loggedUser,
+    setShowEvent,
+    targetEvent,
+    setTargetEvent
+  }
 
 
-    // PRE-RETURN (package drilled props)
-    const eventsDashProps = {
-        loggedUser,
-        setShowEvent,
-        targetEvent,
-        setTargetEvent
-    }
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 col-md-5">
+          <EventsDashVolunteers events={eventsObj} {...eventsDashProps} />
+        </div>
 
+        <div className="col-12 col-md-7">
+          <h3>Personal Stats</h3>
+          <br />
 
-    return (
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-12 col-md-5">
-              <EventsDashVolunteers events={eventsObj} {...eventsDashProps} />
-            </div>
-
-            <div className="col-12 col-md-7">
-              <h3>Personal Stats</h3>
-              <br />
-
-              {
-                eventsObj.upcomings.length === 0
-                  ? <p>You are not registered to volunteer at any upcoming events. Visit the Events page to find out more!</p>
-                  : null
-              }
-              <p>You've got {volunteeredTime} volunteer hours!</p>
-              {
-                eventsObj.pasts.length < 1
-                  ? <p>You haven't participated in any events yet.</p>
-                  : eventsObj.pasts.length > 1
-                    ? <p>So far, you've participated in {eventsObj.pasts.length} events.</p>
-                    : <p>So far, you've participated in {eventsObj.pasts.length} event.</p>
-              }
-            </div>
-          </div>
           {
-              showEvent
-              ? <EventCard
-                  loggedUser={loggedUser}
-                  event={targetEvent}
-                  setFeedback={setFeedback}
-                  reloadParent={reloadDashboard}
-                  setReloadParent={setReloadDashboard}
-                  hideEvent={hideEvent}
-                />
+            eventsObj.upcomings.length === 0
+              ? <p>You are not registered to volunteer at any upcoming events. Visit the Events page to find out more!</p>
               : null
           }
-
-          {/* {
-              eventsList.map(event => <EventPreviewCard
-                  key={event.event_id + event.event_start + event.event_end}
-                  loggedUser={loggedUser}
-                  event={event}
-                  setShowEvent={setShowEvent}
-                  targetEvent={targetEvent}
-                  setTargetEvent={setTargetEvent}
-              />)
-          } */}
-          {/* <br></br>
-          <br></br>
-          <h3>Important Pursuit Events</h3>
+          <p>You've got {volunteeredTime} volunteer hours!</p>
           {
-              importantEvents.map(event => <EventPreviewCard
-                  key={event.event_end + event.event_start + event.event_id}
-                  loggedUser={loggedUser}
-                  event={event}
-                  setShowEvent={setShowEvent}
-                  targetEvent={targetEvent}
-                  setTargetEvent={setTargetEvent}
-              />)
-          } */}
-
+            eventsObj.pasts.length < 1
+              ? <p>You haven't participated in any events yet.</p>
+              : eventsObj.pasts.length > 1
+                ? <p>So far, you've participated in {eventsObj.pasts.length} events.</p>
+                : <p>So far, you've participated in {eventsObj.pasts.length} event.</p>
+          }
         </div>
-    )
+      </div>
+      {
+        showEvent
+          ? <EventCard
+            loggedUser={loggedUser}
+            event={targetEvent}
+            setFeedback={setFeedback}
+            reloadParent={reloadDashboard}
+            setReloadParent={setReloadDashboard}
+            hideEvent={hideEvent}
+          />
+          : null
+      }
+
+      <div>
+        <Line data={barData} options={barOptions.options} />
+      </div>
+
+    </div>
+  )
 }
 
 export default Dashboard;
