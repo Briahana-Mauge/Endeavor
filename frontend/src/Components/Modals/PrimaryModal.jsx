79e387165bd0d302a66928998da27d2a9dfbad05
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-05-11 02:05:56 -0400

    implement sticky and absolute positioning to close x of primaryModal

--- frontend/src/Components/Modals/PrimaryModal.jsx
+++ frontend/src/Components/Modals/PrimaryModal.jsx
@@ -19,7 +19,9 @@ export const PrimaryModalContainer = (props) => {
 
           <div className="modal-header">
             <h5 className="modal-title" id="exampleModalLongTitle">{header}</h5>
-            <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={hideEvent}>
+          </div>
+          <div className="g1CloseContainer">
+            <button type="button" className="close sticky-top pt-3" data-dismiss="modal" aria-label="Close" onClick={hideEvent}>
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
