5a4248c30d34d231241c8c441e78caeb178ffb8a
Bpmauge <briahanamauge@pursuit.org>
2020-05-13 17:39:27 -0400

    change color of table

---
+++ frontend/src/Components/Dashboards/Chart_Admin.jsx
@@ -0,0 +1,95 @@
+/*
+ANIME BENSALEM, BRIAHANA MAUGÉ, JOSEPH P. PASAOA
+DashboardVolunteers Component | Capstone App (Pursuit Volunteer Mgr)
+*/
+
+
+/* IMPORTS */
+import React, { useState, useEffect } from 'react';
+import Chart from '../Chart';
+
+export default function Charts(props) {
+  const { chartData } = props;
+  const [chartInterval, setChartInterval] = useState([]);
+  const [chartTotalVolunteerHours, setChartTotalVolunteerHours] = useState([]);
+  const [chartTotalEvents, setChartTotalEvents] = useState([]);
+  const [chartTotalVolunteerSignup, setChartTotalVolunteerSignup] = useState([]);
+
+  useEffect(() => {
+    if (chartData !== undefined) {
+      let month = new Date().getMonth() + 2;
+      let year = new Date().getFullYear() - 1;
+      const datesArr = [];
+      const hoursArr = [];
+      const eventsArr = [];
+      const volunteersArr = [];
+
+      for (let i = 0; i < 12; i++) {
+        let date = '';
+        if (month <= 12) {
+          if (month < 10) {
+            date = '0' + month + '-' + year;
+          } else {
+            date = month + '-' + year;
+          }
+          month += 1;
+        } else {
+          if (month < 22) {
+            date = '0' + (month - 12) + '-' + (year + 1);
+          } else {
+            date = (month - 12) + '-' + (year + 1);
+          }
+          month += 1;
+        }
+        datesArr.push(date);
+      }
+
+      for (let date of datesArr) {
+        let h = null;
+        let e = null;
+        let v = null;
+
+          for (let data of chartData[0].hours) {
+            if (data.date === date) {
+              h = data.hours;
+            }
+          }
+
+          for (let data of chartData[0].events) {
+            if (data.date === date) {
+              e = data.count;
+            }
+          }
+
+          for (let data of chartData[0].volunteers) {
+            if (data.date === date) {
+              v = data.volunteers;
+            }
+          }
+
+        hoursArr.push(h);
+        eventsArr.push(e);
+        volunteersArr.push(v)
+      }
+
+      setChartInterval(datesArr);
+      setChartTotalEvents(eventsArr);
+      setChartTotalVolunteerHours(hoursArr);
+      setChartTotalVolunteerSignup(volunteersArr)
+    }
+  }, [chartData]);
+
+
+  return (
+    <>
+      <Chart xAxes={chartInterval} data={chartTotalVolunteerSignup}
+        title={"New Volunteer Signups for a Year"} xText={"Months"} yText={'Number of Volunteers'} color={'rgba(0, 210, 0, 1)'} />
+      <br></br>
+      <Chart xAxes={chartInterval} data={chartTotalVolunteerHours}
+        title={"Total Volunteer Hours in for a Year"} xText={"Months"} yText={'Number of Hours'} color={'rgba(255, 99, 132, 1)'} />
+      <br></br>
+      <Chart xAxes={chartInterval} data={chartTotalEvents}
+        title={"Total Events Held in for a Year"} xText={"Months"} yText={'Number of Events'} color={'rgba(155, 49, 117, 1)'} />
+    </>
+  )
+}
