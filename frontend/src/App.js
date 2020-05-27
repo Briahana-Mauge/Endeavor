19ba45a67ac11e8fd98933dca83caaa215465ed1
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-05-27 01:52:40 -0400

    create and passdown as props persistent resultMode states on App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -63,6 +63,10 @@ function App() {
   const [industrySpeaker, setIndustrySpeaker] = useState(false);
   const [publicProfile, setPublicProfile] = useState(false);
 
+  // SEARCH states
+  const [isVolunteerSearchGrided, setIsVolunteerSearchGrided] = useState(false);
+  const [isEventSearchGrided, setIsEventSearchGrided] = useState(false);
+
   // OTHER variables
   const userIs = identifyUser(loggedUser);
   const location = useLocation();
@@ -160,6 +164,10 @@ function App() {
     industrySpeaker, setIndustrySpeaker,
     publicProfile, setPublicProfile
   }
+  const searchProps = {
+    isVolunteerSearchGrided, setIsVolunteerSearchGrided,
+    isEventSearchGrided, setIsEventSearchGrided
+  }
 
 
   /* BUILD LIMITED ACCESS ROUTES */
@@ -170,7 +178,7 @@ function App() {
     fellowsDashboard = <DashboardFellows {...userProps} />,
     volunteersHome = (
       <PrivateGate path='/volunteers' h1='Volunteers List' {...gateProps}>
-        <Volunteers {...userProps} />
+        <Volunteers {...userProps} {...searchProps} />
       </PrivateGate>
     ),
     volunteersProfile = (
@@ -268,7 +276,7 @@ function App() {
         </PrivateGate>
 
         <PrivateGate path='/events' h1='Events List' {...gateProps}>
-          <Events {...userProps} />
+          <Events {...userProps} {...searchProps} />
         </PrivateGate>
 
 
