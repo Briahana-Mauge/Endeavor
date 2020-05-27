8d345cb275c0819f93b570dff037c8dfa42f8fd3
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-05-26 23:57:26 -0400

    create and style volunteer view slider for grid v list

--- frontend/src/Components/Volunteers.jsx
+++ frontend/src/Components/Volunteers.jsx
@@ -32,6 +32,7 @@ export default function Volunteers (props) {
     const [urlTargetSkill, setUrlTargetSkill] = useState(strQueryTargetSkill || '');
     
     const [results, setResults] = useState([]);
+    const [isDisplayModeGrid, setIsDisplayModeGrid] = useState(false);
     const [skillsList, setSkillsList] = useState([]);
     const [targetVolunteerId, setTargetVolunteerId] = useState(null);
     const [displayTargetUser, setDisplayTargetUser] = useState(false);
@@ -91,35 +92,49 @@ export default function Volunteers (props) {
 
     return (
         <>
-            <div className="Search">
-                <form className='form-inline' onSubmit={handleSubmit}>
-                    <input className='form-control mb-2 mr-sm-2 min-w-25' type='text' 
-                        placeholder='Search' value={searchValue} onChange={e => { setUrlSearchValue(e.target.value) }} />
+            <form className='form-inline' onSubmit={handleSubmit}>
+                <input className='form-control mb-2 mr-sm-2 min-w-25' type='text'
+                    placeholder='Search' value={searchValue} onChange={e => { setUrlSearchValue(e.target.value) }} />
 
-                    <select className='form-control mb-2 mr-sm-2' value={filter} onChange={e => setUrlFilter(e.target.value)}>
-                        <option value=''>Choose a search filter</option>
-                        <option value='name'>Name</option>
-                        <option value='v_email'>Email</option>
-                        <option value='company'>Company</option>
-                    </select>
+                <select className='form-control mb-2 mr-sm-2' value={filter} onChange={e => setUrlFilter(e.target.value)}>
+                    <option value=''>Choose a search filter</option>
+                    <option value='name'>Name</option>
+                    <option value='v_email'>Email</option>
+                    <option value='company'>Company</option>
+                </select>
 
-                    <select className='form-control mb-2 mr-sm-2' value={targetSkill} onChange={e => setUrlTargetSkill(e.target.value)}>
-                        <option value=''>-- Skill --</option>
-                        { skillsList.map(skill => <option key={skill.skill + skill.skill_id} value={skill.skill}>{skill.skill}</option>) }
-                    </select>
+                <select className='form-control mb-2 mr-sm-2' value={targetSkill} onChange={e => setUrlTargetSkill(e.target.value)}>
+                    <option value=''>-- Skill --</option>
+                    { skillsList.map(skill => <option key={skill.skill + skill.skill_id} value={skill.skill}>{skill.skill}</option>) }
+                </select>
 
-                    <button className='btn btn-primary mb-2'>Search</button>
-                </form>
+                <button className='btn btn-primary mb-2'>Search</button>
+            </form>
 
-                <div className='g1VolunteerResults d-flex flex-wrap'>
-                    {results.map(volunteer => <VolunteerCard
-                            key={volunteer.v_id + volunteer.v_first_name + volunteer.v_last_name}
-                            volunteer={volunteer}
-                            setDisplayTargetUser={setDisplayTargetUser}
-                            setTargetVolunteerId={setTargetVolunteerId}
-                        />
-                    )}
+            {/* List or grid toggle */}
+            <div className='g1ToggleListOrGrid'>
+                View Mode:
+                <label class='g1LeftLabel' for="customSwitch1">List</label>
+                <div class="custom-control custom-switch">
+                    <input
+                        type="checkbox"
+                        checked={isDisplayModeGrid}
+                        onClick={() => setIsDisplayModeGrid(!isDisplayModeGrid)}
+                        class="custom-control-input"
+                        id="customSwitch1" />
+                    <label class="custom-control-label" for="customSwitch1">Grid</label>
                 </div>
+            </div>
+
+            {/* Search results */}
+            <div className={`g1VolunteerResults ${isDisplayModeGrid ? 'g1ListResults' : 'g1GridResults'}`}>
+                {results.map(volunteer => <VolunteerCard
+                        key={volunteer.v_id + volunteer.v_first_name + volunteer.v_last_name}
+                        volunteer={volunteer}
+                        setDisplayTargetUser={setDisplayTargetUser}
+                        setTargetVolunteerId={setTargetVolunteerId}
+                    />
+                )}
             </div>
 
             <PrimaryModalContainer header={'Volunteer Profile'} className='g1VolunteerModal' runOnModalClose={hideVolunteer}>
