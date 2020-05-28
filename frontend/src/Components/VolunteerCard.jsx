d0e1bdc0341c145bd0eff9555675cb80adbf5be8
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-05-28 13:13:44 -0400

    continue building vol results grid cards

--- frontend/src/Components/VolunteerCard.jsx
+++ frontend/src/Components/VolunteerCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
+import IconEmail from './IconEmail';
+
 
 const VolunteerCard = (props) => {
     const history = useHistory();
@@ -10,10 +12,37 @@ const VolunteerCard = (props) => {
         props.setTargetVolunteerId(props.volunteer.v_id);
     }
 
-    const skills = [];
-    for (let i = 0; i < Math.min(3, props.volunteer.skills.length); i++) {
-        skills.push(props.volunteer.skills[i]);
+    const shownSkills = [];
+    let skillsPool = [...props.volunteer.skills];
+    let duration = props.volunteer.skills.length > 3
+        ? 3
+        : props.volunteer.skills.length;
+    for (let i = 0; i < duration; i++) {
+        // eventually we'd rather have the volunteers rank their skills and display here the top 3 instead of random
+        let selectedSkill = skillsPool.splice(Math.floor(Math.random() * skillsPool.length), 1)[0];
+        switch (selectedSkill) {
+            case 'Resumes, LinkedIn, and Cover Letters':
+                shownSkills.push('Resumes, Cover Letters, LinkedIn');
+                break;
+            case 'Project Management: Roles, Tools, and Best Practices':
+                shownSkills.push('Project Management');
+                break;
+            case 'Product Design, UX, and Prototyping':
+                shownSkills.push('UX, Design, and Protoyping');
+                break;
+            case 'Talking About Tech Projects in Interviews':
+                shownSkills.push('Interview Tech Talk');
+                break;
+            default:
+                shownSkills.push(selectedSkill);
+        }
     }
+    const listSkills = shownSkills.map((skill, index) => <li key={index+skill}>{skill}</li>);
+    if (props.volunteer.skills.length > 3) {
+        listSkills.push(<li className='g1MoreItemsMsg'><i>+ {props.volunteer.skills.length - 3} more . . .</i></li>)
+    }
+
+
 
     /* next event is an string containing the event id and topic, separated by ' &$%& '
         index0: event ID
@@ -35,9 +64,7 @@ const VolunteerCard = (props) => {
                 >
                     {props.volunteer.v_first_name} {props.volunteer.v_last_name}
                 </h4>
-                <div className='g1VolResultCard__Email'>
-                    {props.volunteer.v_email}
-                </div>
+                <IconEmail email={props.volunteer.v_email} className='g1VolResultCard__IconEmail' />
                 <div className='g1VolResultCard__Job'>
                     {props.volunteer.company}<br />
                     {props.volunteer.title}
@@ -50,15 +77,17 @@ const VolunteerCard = (props) => {
                     data-toggle="modal"
                     data-target="#primaryModal"
                 />
-                <div className='card-body d-flex flex-column p-3'>
+                <ul className='g1VolResultCard__SkillsInterests'>
+                    {listSkills}
+                </ul>
 
 
 
-                    <p className='card-text'></p>
+                    {/* <p className='card-text'></p>
                     <h5>Skills:</h5>
                     <p className='card-text'>
                         { skills.map((skill, index) => <span className='d-block' key={index+skill}>{skill}</span>) }
-                    </p>
+                    </p> */}
                     {   props.volunteer.next_event
                         ?   <div className='card-text'> Next Event: <br />
                                 <span style={{ cursor: 'pointer' }} onClick={e => history.push(`/event/${nextEvent[0]}`)}>{nextEvent[1]}</span>
@@ -68,8 +97,8 @@ const VolunteerCard = (props) => {
                     <div className='text-left mt-auto'>
                         <button className='btn btn-primary' onClick={viewProfile} data-toggle="modal" data-target="#primaryModal">See Profile</button>
                     </div>
-                </div>
-                </div>
+                {/* </div> */}
+            </div>
         </div>
     );
 }
