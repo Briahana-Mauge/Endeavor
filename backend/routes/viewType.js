3e7eb679f964ef256b6fbfb02fb0577f3cfe3e16
AminesCodes <aminebensalem@pursuit.org>
2020-06-03 02:39:07 -0400

    Backend - Clean an unused code

--- backend/routes/viewType.js
+++ backend/routes/viewType.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const { hashPassword, comparePasswords } = require('../auth/helpers');
-
 const adminQueries = require('../queries/admin');
 const volunteersQueries = require('../queries/volunteers');
 const fellowsQueries = require('../queries/fellows');
