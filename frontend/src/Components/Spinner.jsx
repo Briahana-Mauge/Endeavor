bd491984964e8cb57fc888290a4a727f8a7a35ea
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-06-07 12:50:00 -0400

    fix size type to number instead of string

--- frontend/src/Components/Spinner.jsx
+++ frontend/src/Components/Spinner.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { AtomSpinner } from 'react-epic-spinners';
 
 const Spinner = (props) => {
-    let size='300';
-    if(props.size){
-        size = props.size
-    } 
-    
+    let size = 300;
+    if (props.size) {
+        size = props.size;
+    }
+
     return (
         <AtomSpinner className = {`spinner ${props.className}`} color = "#8d8558" size = {size}/>
-       
-    )
+    );
 }
 
 export default Spinner;
