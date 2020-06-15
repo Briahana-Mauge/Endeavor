34695035a96a5ed11195fa61a51e8f83ccc324c1
AminesCodes <aminebensalem@pursuit.org>
2020-06-14 22:37:09 -0400

    Frontend - Issue #136 bug fix

--- frontend/src/Components/Dashboards/NewVolunteersDash/NewVolunteersDash.jsx
+++ frontend/src/Components/Dashboards/NewVolunteersDash/NewVolunteersDash.jsx
@@ -5,7 +5,7 @@ NewVolunteersDash Component | Capstone App (Pursuit Volunteer Mgr)
 
 
 /* IMPORTS */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import UIModule from '../../UIModule';
@@ -15,47 +15,72 @@ import VolunteerPreviewCard from './VolunteerPreviewCard';
 const NewVolunteersDash = (props) => {
   const { newVolunteers, reloadDashboard, setReloadDashboard, setFeedback } = props;
 
+  const [ volunteerIndex, setVolunteerIndex ] = useState(0);
+  const [ slideIndicators, setSlideIndicators ] = useState([]);
+  const [ slides, setSlides ] = useState([]);
 
+  useEffect(() => {
+    const slideIndicator = [];
+    const slidesList = newVolunteers.map(( volunteer, index ) => {
+        const { v_first_name, v_last_name, v_id } = volunteer;
+  
+        const volunteersInitials = v_first_name.slice(0, 1) + v_last_name.slice(0, 1);
+        slideIndicator.push(index === volunteerIndex
+          ? <li
+              data-target="#newVolunteersSlideshow"
+              data-slide-to={`${index}`}
+              className="active"
+              key={'newVolIndicator' + index}
+              onClick={e => setVolunteerIndex(index)}
+            >
+              <div>{volunteersInitials}</div>
+            </li>
+          : <li
+              data-target="#newVolunteersSlideshow"
+              data-slide-to={`${index}`}
+              key={'newVolIndicator' + index}
+              onClick={e => setVolunteerIndex(index)}
+            >
+              <div>{volunteersInitials}</div>
+            </li>
+        );
+        return(
+          <div
+            className={index === volunteerIndex ? "carousel-item active" : "carousel-item"}
+            key={v_first_name + v_last_name + v_id}
+          >
+            <VolunteerPreviewCard volunteer={volunteer} acceptVolunteer={acceptVolunteer} dismissVolunteer={nextVolunteer}/>
+          </div>
+        );
+    });
+
+    setSlideIndicators(slideIndicator);
+    setSlides(slidesList);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [newVolunteers, reloadDashboard, volunteerIndex]);
+
+  
   const acceptVolunteer = async (id) => {
     try {
         await axios.patch(`/api/volunteers/confirm/${id}`);
+        if (volunteerIndex === newVolunteers.length - 1) {
+          setVolunteerIndex(0)
+        }
         setReloadDashboard(!reloadDashboard);
     } catch (err) {
         setFeedback(err)
     }
   }
 
-  const slideIndicators = [];
-  const slides = newVolunteers.map(( volunteer, index ) => {
-      const { v_first_name, v_last_name, v_id } = volunteer;
+  const previousVolunteer = () => {
+    const newIndex = volunteerIndex - 1 >= 0 ? volunteerIndex - 1 : newVolunteers.length - 1;
+    setVolunteerIndex(newIndex);
+  }
 
-      const volunteersInitials = v_first_name.slice(0, 1) + v_last_name.slice(0, 1);
-      slideIndicators.push(index === 0
-        ? <li
-            data-target="#newVolunteersSlideshow"
-            data-slide-to={`${index}`}
-            className="active"
-            key={'newVolIndicator' + index}
-          >
-            <div>{volunteersInitials}</div>
-          </li>
-        : <li
-            data-target="#newVolunteersSlideshow"
-            data-slide-to={`${index}`}
-            key={'newVolIndicator' + index}
-          >
-            <div>{volunteersInitials}</div>
-          </li>
-      );
-      return(
-        <div
-          className={index === 0 ? "carousel-item active" : "carousel-item"}
-          key={v_first_name + v_last_name + v_id}
-        >
-          <VolunteerPreviewCard volunteer={volunteer} acceptVolunteer={acceptVolunteer}/>
-        </div>
-      );
-  });
+  const nextVolunteer = () => {
+    const newIndex = volunteerIndex + 1 >= newVolunteers.length ? 0 : volunteerIndex + 1;
+    setVolunteerIndex(newIndex);
+  }
 
 
   return (
@@ -75,11 +100,11 @@ const NewVolunteersDash = (props) => {
                 <div className="g1CarouselArrows">
                   <div className="g1CarouselArrowsInterior col-lg-3 pl-0 pl-lg-3 ml-lg-2">
                     <div>
-                      <a className="carousel-control-prev" href="#newVolunteersSlideshow" role="button" data-slide="prev">
+                      <a className="carousel-control-prev" href="#newVolunteersSlideshow" role="button" data-slide="prev" onClick={previousVolunteer}>
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span className="sr-only">Previous</span>
                       </a>
-                      <a className="carousel-control-next" href="#newVolunteersSlideshow" role="button" data-slide="next">
+                      <a className="carousel-control-next" href="#newVolunteersSlideshow" role="button" data-slide="next" onClick={nextVolunteer}>
                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
                         <span className="sr-only">Next</span>
                       </a>
